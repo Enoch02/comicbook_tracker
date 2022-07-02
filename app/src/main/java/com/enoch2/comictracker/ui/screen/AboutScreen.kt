c8f6ab585d426eefb478fd3905fecb2bc13415d7
Enoch <adesanyaenoch@gmail.com>
2022-07-02 13:12:27 +0100

    - Modify about screen

--- app/src/main/java/com/enoch2/comictracker/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/enoch2/comictracker/ui/screen/AboutScreen.kt
@@ -5,8 +5,10 @@ import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -43,44 +45,53 @@ fun AboutScreen(navController: NavController) {
             val context = LocalContext.current
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Enoch02"))
 
-            Box(
-                contentAlignment = Alignment.Center,
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Image(
-                        painterResource(R.drawable.ic_launcher_foreground),
-                        contentDescription = "app icon",
-                        alignment = Alignment.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Text(
-                        "version $VERSION_NAME",
-                        textAlign = TextAlign.Center,
-                        fontSize = 15.sp,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    )
-                    Divider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp, bottom = 10.dp),
-                        thickness = 0.5.dp,
-                    )
-                    Text(
-                        stringResource(R.string.designed_by_txt),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 15.dp)
-                    )
-                    Text(
-                        stringResource(R.string.github),
-                        textAlign = TextAlign.Center,
-                        style = TextStyle(textDecoration = TextDecoration.Underline),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    )
-                    SelectionContainer {
+                Card(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    Column(modifier = Modifier.padding(vertical = 5.dp)) {
+                        Image(
+                            painterResource(R.drawable.ic_launcher_foreground),
+                            contentDescription = "app icon",
+                            alignment = Alignment.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Text(
+                            "version $VERSION_NAME",
+                            textAlign = TextAlign.Center,
+                            fontSize = 15.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
+                }
+
+                Card(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    Column(modifier = Modifier.padding(vertical = 5.dp)) {
+                        Text(
+                            stringResource(R.string.designed_by_txt),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 15.dp)
+                        )
+                        Text(
+                            stringResource(R.string.github),
+                            textAlign = TextAlign.Center,
+                            style = TextStyle(textDecoration = TextDecoration.Underline),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
                         Text(
                             stringResource(R.string.github_link),
                             textAlign = TextAlign.Center,
